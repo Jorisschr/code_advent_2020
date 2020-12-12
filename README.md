3b304ab91255b71f02004882053fe6ab4bd446db
joris@dataroots.io <joris@dataroots.io>
2020-12-12 22:19:03 +0100

    readme, gitignore, leaderboard

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of code 2020
+All python scripts should be run from the top directory.
+
+## Generate leaderboard statistics
+To generate a leaderboard overview:
+- Download the data from your private leaderboard.
+    - Go to https://adventofcode.com/2020/leaderboard/private
+    - *[View] > [API] > [JSON]*
+    - Save the data in the data folder as "*leaderboard.json*"
+- Run ```python .\src\leaderboard.py```
+- Your csv will be saved in the data folder
--- src/leaderboard.py
+++ src/leaderboard.py
@@ -1,6 +1,54 @@
 import json
-import pprint
+import pandas as pd
+from pprint import pprint
+from datetime import datetime
 
-with open("./data/leaderboard.json", "r") as f:
-    leaderboard = json.load(f)
-pprint.pprint(leaderboard)
+
+def parse_input() -> dict:
+    """Parse the json containing the leaderboard data
+
+    Returns
+    -------
+    dict
+        dictionary containing the data
+    """
+    with open("./data/leaderboard.json", "r") as f:
+        leaderboard = json.load(f)
+    return leaderboard
+
+
+def create_overview(lb: dict) -> pd.DataFrame:
+    """Create an overview of the leaderboard
+
+    Parameters
+    ----------
+    lb : dict
+        dictionary containing the leaderboard information
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe containing the overview
+    """
+    rows = []
+    for key in lb["members"]:
+        d = lb["members"][key]["completion_day_level"]
+        for k_1 in d:
+            for k_2 in d[k_1]:
+                rows.append(
+                    [
+                        lb["members"][key]["name"],
+                        int(k_1),
+                        int(k_2),
+                        datetime.fromtimestamp(int(d[k_1][k_2]["get_star_ts"])),
+                    ]
+                )
+    df = pd.DataFrame(data=rows, columns=["name", "problem", "part", "time"])
+    return df.sort_values(by="time")
+
+
+if __name__ == "__main__":
+    lb = parse_input()
+    df = create_overview(lb)
+
+    df.to_csv("./data/leaderboard.csv", index=False)
